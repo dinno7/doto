1119ed2094c2f92cab3644a185a21eb8d772b1be
dinno <tahadlrb7@gmail.com>
2026-06-21 21:32:41 +0330

    feat(domain/vo): impl FromStr & Display traits for Todo status

--- src/domain/vo/todo_status.rs
+++ src/domain/vo/todo_status.rs
@@ -1,3 +1,7 @@
+use std::{fmt::Display, str::FromStr};
+
+use crate::domain::errors::todo::TodoError;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum TodoStatus {
     Pending,
@@ -9,5 +13,30 @@ pub enum TodoStatus {
 impl TodoStatus {
     fn is_done(&self) -> bool {
         *self == TodoStatus::Done
+    }
+}
+
+impl FromStr for TodoStatus {
+    type Err = TodoError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().trim() {
+            "pending" => Ok(TodoStatus::Pending),
+            "in_progress" => Ok(TodoStatus::InProgress),
+            "done" => Ok(TodoStatus::Done),
+            "cancelled" => Ok(TodoStatus::Cancelled),
+            _ => Err(TodoError::InvalidPriority(s.to_string())),
+        }
+    }
+}
+
+impl Display for TodoStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let status_string = match self {
+            TodoStatus::Pending => "pending",
+            TodoStatus::InProgress => "in_progress",
+            TodoStatus::Done => "done",
+            TodoStatus::Cancelled => "cancelled",
+        };
+        write!(f, "{}", status_string)
     }
 }
