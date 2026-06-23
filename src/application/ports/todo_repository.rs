ea7921306e7266df5623ec2eeeded8fabb6e7c81
dinno <tahadlrb7@gmail.com>
2026-06-24 01:13:28 +0330

    feat(app/ports): add delete_by_id for todos

--- src/application/ports/todo_repository.rs
+++ src/application/ports/todo_repository.rs
@@ -3,8 +3,9 @@ use async_trait::async_trait;
 use crate::{application::errors::ApplicationError, domain::entities::todo::Todo};
 
 #[async_trait]
-pub trait TodoRepository {
+pub trait TodoRepository: Send + Sync {
     async fn create(&self, todo: Todo) -> Result<(), ApplicationError>;
     async fn get_all(&self) -> Result<Vec<Todo>, ApplicationError>;
     async fn get_all_with_tags(&self) -> Result<Vec<Todo>, ApplicationError>;
+    async fn delete_by_id(&self, todo_id: u64) -> Result<bool, ApplicationError>;
 }
