2603e7be048ae8c7a2ca0b2330704b551820d6c7
dinno <tahadlrb7@gmail.com>
2026-06-24 04:01:40 +0330

    feat(app/port): add delete methods to todo repo

--- src/application/ports/todo_repository.rs
+++ src/application/ports/todo_repository.rs
@@ -1,6 +1,9 @@
 use async_trait::async_trait;
 
-use crate::{application::errors::ApplicationError, domain::entities::todo::Todo};
+use crate::{
+    application::errors::ApplicationError,
+    domain::{entities::todo::Todo, vo::todo_status::TodoStatus},
+};
 
 #[async_trait]
 pub trait TodoRepository: Send + Sync {
@@ -8,4 +11,6 @@ pub trait TodoRepository: Send + Sync {
     async fn get_all(&self) -> Result<Vec<Todo>, ApplicationError>;
     async fn get_all_with_tags(&self) -> Result<Vec<Todo>, ApplicationError>;
     async fn delete_by_id(&self, todo_id: u64) -> Result<bool, ApplicationError>;
+    async fn delete_all(&self) -> Result<u64, ApplicationError>;
+    async fn delete_all_by_status(&self, todo_status: TodoStatus) -> Result<u64, ApplicationError>;
 }
